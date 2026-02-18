bfdb0d19ccf3e8a36eb96ac4478c9a5f4cc246db
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-18 15:51:55 +0600

    Design Cashout Form and Logical Implementation

---
+++ script/cashout.js
@@ -0,0 +1,10 @@
+document.getElementById("cashout-btn").addEventListener("click", function(){
+    // 1 -  get the agent number & validate
+    // 2 - get the amount, validate, convert to number
+    // 3 - get current balance, validate , conver to number
+    // 4 - Calcualte new balance
+    // 5 - Get the pin and verify
+    // 5.1 - true: show an alert > set Balance
+    // 5.2 - false: show an error alert > return
+    
+})
