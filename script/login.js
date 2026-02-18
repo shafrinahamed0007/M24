8c4cbea2675dc09d1bb9ecaf09ea70522d05c00c
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-18 13:00:07 +0600

    Implement login logic and navaigate to home page

--- script/login.js
+++ script/login.js
@@ -1,1 +1,21 @@
-console.log("Login JS Connected");
+document.getElementById("login-btn").addEventListener("click", function () {
+  // 1. get the mobile number input
+  // 2. get the pin
+  // 3. match pin & mobile
+  // 3.1. true::> homepage
+  // 3.2. false::> return
+
+  const numberInput = document.getElementById("input-number");
+  const contactNumber = numberInput.value;
+
+  const pinInput = document.getElementById("input-pin");
+  const pin = pinInput.value;
+
+  if (contactNumber == "01872437708" && pin == "1234") {
+    alert("login success");
+  }else{
+    alert("login failed");
+    // window.location.replace("/home.html")
+    window.location.assign("/home.html");
+  }
+});
